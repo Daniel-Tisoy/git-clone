3fcc647ae0100627e33d4cd9d76e54567e730f03
dtisoy <danieltisoy2001@gmail.com>
2023-03-02 08:34:52 -0500

    create: git repository class, and some path manipulation methods

---
+++ libwyag.py
@@ -0,0 +1,94 @@
+import argparse # to parse command-line arguments
+import collections #container types
+import configParser # read and write microsoft's INI format
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch # filename matching (to match .gitignore)
+import hashlib # to use the SHA-1 function
+from math import ceil
+import os
+import re
+import sys
+import zlib
+
+argparser = argparse.ArgumentParser(description="The stupidest content tracker")
+
+argsubparsers = argparser.add_subparser(title="Commands", dest="command")
+argsubparser.required = True
+
+def main(argv=sys.argv[1:]):
+    args = arparser.parse_args(argv)
+    match args.command:
+        case "add" : cmd_add(args)
+        case "cat-file" : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout" : cmd_checkout(args)
+        case "commit" : cmd_commit(args)
+        case "hash-object" : cmd_hash_object(args)
+        case "init" : cmd_init(args)
+        case "log" : cmd_log(args)
+        case "ls-files" : cmd_ls_files(args)
+        case "ls-tree" : cmd_ls_tree(args)
+        case "rev-parse" : cmd_rev_parse(args)
+        case "rm" : cmd_rm(args)
+        case "show-ref" : cmd_show_ref(args)
+        case "status" : cmd_status(args)
+        case "tag" : cmd_tag(args)
+        case _ : print("Bad command")
+
+class GitRepository(object):
+    """A git repository"""
+    workthree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.workthree = path
+        self.gitdir = os.path.join(path, ".git")
+        
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a git repository %s" % path)
+
+        # read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+        
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsuported repositoryformatversion %s" % vers)
+
+
+def repo_path(repo, *path):
+    """Compute path under repo's gitdir"""
+
+    return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path, but create dirname(*path) if absent.  For
+    example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
+    .git/refs/remotes/origin."""
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir: 
+        os.makedir(path)
+        return path
+
+    else: return None
