f0f0c37db25a601baf4dbed1fd3a3147383ef9a9
dtisoy <danieltisoy2001@gmail.com>
2023-03-02 09:12:56 -0500

    update libwyag: add create repo function

--- libwyag.py
+++ libwyag.py
@@ -38,12 +38,12 @@ def main(argv=sys.argv[1:]):
 
 class GitRepository(object):
     """A git repository"""
-    workthree = None
+    worktree = None
     gitdir = None
     conf = None
 
     def __init__(self, path, force=False):
-        self.workthree = path
+        self.worktree = path
         self.gitdir = os.path.join(path, ".git")
         
         if not (force or os.path.isdir(self.gitdir)):
@@ -88,7 +88,49 @@ def repo_dir(repo, *path, mkdir=False):
             raise Exception("Not a directory %s" % path)
 
     if mkdir: 
-        os.makedir(path)
+        os.makedirs(path)
         return path
 
     else: return None
+
+def create_repo(path):
+    """create a new repository at path"""
+    
+    repo = GitRepository(path, force=True)
+
+    # First, we make sure the path either doesn't exist or is an empty dir.
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("%s is not a directory" % path)
+        if not os.path.exists(repo.gitdir) and os.path.listdir(repo.gitdir):
+            raise Exception("%s is not empty" % path)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
+
+    #.git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+        
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+    
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+    
+    return ret
